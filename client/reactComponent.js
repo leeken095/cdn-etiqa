45664407703758fa1c76ee6d1558f4d093dfeb35
Lee Ken <ken.lee@lollipoplab.io>
2020-01-23 21:14:31 +0800

    First Commit

---
+++ client/reactComponent.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const e = React.createElement;
+
+class UserList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users:[
+        {
+          name:"Lee Ken"
+        },
+        {
+          name:"John"
+        }
+      ]
+    }
+  }
+
+  componentDidMount(){
+    console.log("hi");
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.users.map((aUser)=>{
+          return (
+            <div>{aUser.name}</div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+
+const domContainer = document.getElementById("react-container");
+ReactDOM.render(e(UserList), domContainer);
